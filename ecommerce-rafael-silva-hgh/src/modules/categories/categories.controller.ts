b86cc11bd1bbc893881cd70a56218ad18320b495
Rafael Silva <kingorren1@gmail.com>
2025-06-12 12:26:41 -0600

    categories completo

---
+++ ecommerce-rafael-silva-hgh/src/modules/categories/categories.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Body } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+
+@Controller('categories')
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Get('seeder')
+  addCategory() {
+    return this.categoriesService.addCategory();
+  }
+
+  @Get()
+  getCategories() {
+    return this.categoriesService.getCategories();
+  }
+}
