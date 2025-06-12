dac707fd76f5bc3375e7ef5324d2788c5a779e18
Rafael Silva <kingorren1@gmail.com>
2025-06-12 10:01:56 -0600

    .env

--- ecommerce-rafael-silva-hgh/src/app.module.ts
+++ ecommerce-rafael-silva-hgh/src/app.module.ts
@@ -4,28 +4,21 @@ import { ProductsModule } from './modules/products/products.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import typeOrmConfig from './config/typeorm';
-import { DataSourceOptions } from 'typeorm';
+import typeorm from './config/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [typeOrmConfig],
-    }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService): DataSourceOptions => {
-        const config = configService.get<DataSourceOptions>('typeorm');
-        if (!config) {
-          throw new Error('TypeORM configuration not found');
-        }
-        return config;
-      },
-    }),
     UsersModule,
     ProductsModule,
     AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => config.get('typeorm')!,
+    }),
   ],
   controllers: [],
   providers: [],
