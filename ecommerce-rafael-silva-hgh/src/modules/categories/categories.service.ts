9038849f1de3eedf4990f1643f3c73a659433ea5
Rafael Silva <kingorren1@gmail.com>
2025-06-12 12:05:53 -0600

    seeder categories

---
+++ ecommerce-rafael-silva-hgh/src/modules/categories/categories.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Categories } from './categories.entity';
+import { Repository } from 'typeorm';
+import * as data from '../../../data.json';
+
+@Injectable()
+export class CategoriesService {
+  constructor(
+    @InjectRepository(Categories)
+    private readonly categoriesRepository: Repository<Categories>,
+  ) {}
+
+  async addCategory() {
+    const categoryNames = new Set(data.map((element) => element.category));
+    const categoriesArray = Array.from(categoryNames);
+    const categories = categoriesArray.map((category) => ({ name: category }));
+
+    await this.categoriesRepository.upsert(categories, ['name']);
+    return 'This action adds a new category';
+  }
+
+  getCategories() {
+    return this.categoriesRepository.find();
+  }
+}
